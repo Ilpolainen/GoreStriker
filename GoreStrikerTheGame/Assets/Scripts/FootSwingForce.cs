003f43a133eafb7902aeaf8882e411cbaa9def9d
Ilpolainen <pohjolanilmari@gmail.com>
2016-09-13 00:52:58 +0300

    Initial work

---
+++ GoreStrikerTheGame/Assets/Scripts/FootSwingForce.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootSwingForce : MonoBehaviour {
+
+	Vector2 joystickInput;
+	Rigidbody rb;
+	public float swingPower;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetJoystickInput ();
+		AddForce ();
+	}
+
+	void GetJoystickInput() {
+		joystickInput = InputManager.GetLeftStickInput ();
+	}
+
+	void AddForce() {
+		rb.AddForce (joystickInput * swingPower);
+	}
+}
---
+++ GoreStrikerTheGame/Assets/Scripts/LegMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LegMovement : MonoBehaviour {
+
+
+	public GameObject rightKnee;
+	public GameObject leftKnee;
+	Rigidbody rightKneeRigidbody;
+	Rigidbody leftKneeRigidbody;
+
+	private GameObject swingingKnee;
+	private Rigidbody swingingKneeRigidbody;
+
+	private bool rightKneeGrounded;
+
+	Vector2 joystickInput;
+	public float swingPower;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rightKneeRigidbody = rightKnee.GetComponent<Rigidbody> ();
+		leftKneeRigidbody = leftKnee.GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GetJoystickInput ();
+		CheckConditions ();
+		AddForce ();
+	}
+
+	void GetJoystickInput() {
+		joystickInput = InputManager.GetLeftStickInput ();
+	}
+
+	void CheckConditions() {
+		//Calls statemachine
+	}
+		
+
+	void AddForce() {
+		swingingKneeRigidbody.AddForce (joystickInput * swingPower);
+	}
+
+	public void SwapSwingingKnee() {
+		if (!rightKneeGrounded) {
+			swingingKnee = rightKnee;
+			swingingKneeRigidbody = rightKneeRigidbody;
+			rightKneeGrounded = true;
+		} else {
+			swingingKnee = leftKnee;
+			swingingKneeRigidbody = swingingKneeRigidbody;
+			rightKneeGrounded = false;
+		}
+	} 
+
+	void SwapKneeGroundJoint() {
+		
+	}
+
+
+}
